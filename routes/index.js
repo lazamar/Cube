357d7c0c8c264c44262c3d01520ac3b05e038a76
lazamar <lazaronijunior@gmail.com>
2016-08-28 20:38:57 +0100

    Use templating engine

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,7 @@ app.engine('.hbs', exphbs({ extname: '.hbs', defaultLayout: 'admin' }));
 app.set('view engine', '.hbs');
 
 app.get('/', (req, res) => {
-	res.render('start', { title: 'Hey!', message: 'Hello there!' });
+	res.render('index');
 });
 
 app.listen(env.port, () => console.log('Listening on port', env.port));
